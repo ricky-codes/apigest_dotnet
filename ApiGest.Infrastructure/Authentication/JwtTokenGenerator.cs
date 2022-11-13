d8f21b051958278e5c36c1d7a23eff49891dd9d7
ricky-codes <ricardu12345@gmail.com>
2022-11-13 17:51:05 +0000

    [Added] Added jwt user secrets to Api Project

--- ApiGest.Infrastructure/Authentication/JwtTokenGenerator.cs
+++ ApiGest.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -5,23 +5,26 @@ using System.Security.Claims;
 using System.IdentityModel.Tokens.Jwt;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Options;
 using ApiGest.Application.Common.Interfaces.Services;
 
 public class JwtTokenGenerator : IJwtTokenGenerator{
     
 
     private readonly IDateTimeProvider _dateTimeProvider;
+    private readonly JwtSettings _jwtSettings;
 
-    public JwtTokenGenerator(IDateTimeProvider dateTimeProvider)
-    {
-        _dateTimeProvider = dateTimeProvider;
+
+    public JwtTokenGenerator(IDateTimeProvider dateTimeProvider, IOptions<JwtSettings> jwtOptions){
+        this._dateTimeProvider = dateTimeProvider;
+        this._jwtSettings = jwtOptions.Value;
     }
 
     public string GenerateToken(Guid userId, string firstname, string lastname){
 
         var signingCredentials = new SigningCredentials(
             new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes("super-secret-key")), 
+                Encoding.UTF8.GetBytes(this._jwtSettings.Secret)), 
             SecurityAlgorithms.HmacSha256);
 
         var claims = new [] {
@@ -32,8 +35,9 @@ public class JwtTokenGenerator : IJwtTokenGenerator{
         };
 
         var securityToken = new JwtSecurityToken(
-            issuer: "ApiGest",
-            expires: _dateTimeProvider.UtcNow.AddMinutes(60),
+            issuer: this._jwtSettings.Issuer,
+            audience: this._jwtSettings.Audience,
+            expires: _dateTimeProvider.UtcNow.AddMinutes(this._jwtSettings.ExpiryMinutes),
             claims: claims, 
             signingCredentials: signingCredentials);
 
--- ApiGest.Infrastructure/DependencyInjection.cs
+++ ApiGest.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,6 @@
 namespace ApiGest.Infrastructure;
 
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ApiGest.Application.Common.Interfaces.Authentication;
 using ApiGest.Infrastructure.Authentication;
@@ -7,7 +8,9 @@ using ApiGest.Application.Common.Interfaces.Services;
 using ApiGest.Infrastructure.Services;
 
 public static class DependencyInjection {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services) {
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
+    {
+        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
         services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
         services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
         return services;
